67b9be45771ad5d8b88782ff1a9e4a48b4af6954
赵林 <13840214233@163.com>
2019-04-30 08:02:24 +0800

    1 train.py新增测试 2 test.py修复模型加载bug

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import Net
 from LineDataset import *
+from torch.utils.data import DataLoader
+from train import test
 from torchvision import transforms
 from torch.autograd import Variable
 import os
@@ -9,12 +11,16 @@ if __name__ == '__main__':
     ckpt_path = './check_point/base_model_100.pth'
     composed = transforms.Compose([Normalize(),
                                    ToTensor()])
-    line_dataset = LineDataset(dataset_file_path, transform=composed)
-    model = Net.Net1().cuda()
+    batch_size = 32
+    test_dataset = LineDataset(dataset_file_path, transform=composed)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    model = torch.load(ckpt_path)
     model.eval()
-    for sample in line_dataset:
+
+    for sample in test_dataset:
         img = sample['image']
         img = torch.unsqueeze(img, 0).cuda()
         label = sample['label']
-        pre_label = model(img)
+        pre_label = torch.argmax(model(img), 1)
+        print('label: {}, prediction: {}'.format(label.item(), pre_label.item()))
         pass
--- train.py
+++ train.py
@@ -8,32 +8,51 @@ from torchvision import transforms
 from torch.autograd import Variable
 import os
 
+def train_step(model, batched_sample, class_num):
+    imgs = batched_sample['image'].cuda()
+    labels = batched_sample['label'].long()
+    temp_batch_size = labels.size()[0]
+    ont_hot_labels = Variable(torch.zeros(temp_batch_size, class_num).scatter_(1, labels, 1).float().cuda())
+    imgs = Variable(imgs)
+    pre_labels = model(imgs)
+    loss = criterion(pre_labels, ont_hot_labels)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss
+
+def test(model, data_loader):
+    right_num = torch.Tensor([0]).cuda()
+    all_num = torch.Tensor([0]).cuda()
+    for batched_sample in data_loader:
+        imgs = batched_sample['image'].cuda()
+        labels = batched_sample['label'].long().cuda()
+        pred_lables = model(imgs)
+        pred_lables = torch.argmax(pred_lables, 1, keepdim=True)
+        right_num += (pred_lables == labels).sum()
+        all_num += labels.shape[0]
+    accuracy = (right_num / all_num).cpu()
+    return accuracy
+
 if __name__ == '__main__':
-    dataset_file_path = 'samples_0.txt'
+    trainset_path = 'samples_0.txt'
+    testset_path = 'samples_1.txt'
     ckpt_dir = './check_point'
     composed = transforms.Compose([Normalize(),
                                    ToTensor()])
     batch_size = 32
     class_num = 2
-    line_dataset = LineDataset(dataset_file_path, transform=composed)
-    line_dataloader = DataLoader(line_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_dataset = LineDataset(trainset_path, transform=composed)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_dataset = LineDataset(testset_path, transform=composed)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     model = Net.Net1().cuda()
-
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.1)
     epoch_size = 100
     for epoch in range(epoch_size):
-        for ibatch, sample_batched in enumerate(line_dataloader):
-            imgs = sample_batched['image'].cuda()
-            labels = sample_batched['label'].long()
-            temp_batch_size = labels.size()[0]
-            ont_hot_labels = Variable(torch.zeros(temp_batch_size, class_num).scatter_(1, labels, 1).float().cuda())
-            imgs = Variable(imgs)
-            pre_labels = model(imgs)
-            loss = criterion(pre_labels, ont_hot_labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        for ibatch, batched_sample in enumerate(train_dataloader):
+            loss = train_step(model, batched_sample, class_num)
             # params = model.state_dict()
             if ibatch % 20 == 0:
                 print('Epoch [{} / {}] Batch [{}], loss: {:.6f}'
@@ -43,4 +62,9 @@ if __name__ == '__main__':
                 os.makedirs(ckpt_dir)
             model_path = os.path.join(ckpt_dir, 'base_model_{}.pth'.format(epoch + 1))
             torch.save(model, model_path)
+            accuracy = test(model, test_dataloader)
+            print('Epoch [{} / {}], accuracy: {:.3f}'
+                      .format(epoch + 1, epoch_size, accuracy.item()))
+
+
 
